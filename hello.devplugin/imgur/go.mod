2300ade87b50b52aa2dd9c856c66e45a15c2b8f5
Serg Yash <Snowfireplace@outlook.com>
2023-04-13 11:47:01 +0300

    modified:   .gitignore 	modified:   hello.devplugin/imgur/go.mod 	modified:   hello.devplugin/imgur/go.sum 	modified:   hello.devplugin/imgur/main.go

--- hello.devplugin/imgur/go.mod
+++ hello.devplugin/imgur/go.mod
@@ -2,4 +2,9 @@ module imgur
 
 go 1.19
 
-require gopkg.in/yaml.v3 v3.0.1
+require (
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
--- hello.devplugin/imgur/main.go
+++ hello.devplugin/imgur/main.go
@@ -10,16 +10,20 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"time"
 
-	"gopkg.in/yaml.v3"
+	"github.com/pkg/browser"
 )
 
-var conf configuration
+const version = "0.0.1"
 
-type configuration struct {
-	Client string `yaml:"clientID"`
-	Secret string `yaml:"secret"`
-}
+var im imgur
+var (
+	f        *os.File
+	infoLog  *log.Logger
+	errorLog *log.Logger
+)
+
 type Request struct {
 	Data struct {
 		Link       string `json:"link"`
@@ -27,68 +31,100 @@ type Request struct {
 	} `json:"data"`
 }
 
-func main() {
-	yfile, err := ioutil.ReadFile("./conf.yaml")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = yaml.Unmarshal(yfile, &conf)
-
-	if err != nil {
-
-		log.Fatal(err)
-	}
-
-	// url := "https://api.imgur.com/3/image"
-	// method := "POST"
-	image, err := os.Open("./imgs/9e63c8436ff97c485da4c3b93d84cb62.jpeg")
-	if err != nil {
-		log.Fatal(err)
-	}
-	upload(image)
-
+type imgur struct {
+	Buf        *bytes.Buffer
+	Writer     *multipart.Writer
+	URL        string
+	Deletehash string
 }
-func upload(image io.Reader) (string, string) {
-	r := Request{}
-	var buf = new(bytes.Buffer)
-	writer := multipart.NewWriter(buf)
 
-	part, _ := writer.CreateFormFile("image", "dont care about name")
+func (i *imgur) init() {
+	i.Buf = new(bytes.Buffer)
+	i.Writer = multipart.NewWriter(i.Buf)
+}
+func logConf() {
+
+	f, err := os.OpenFile("info.log", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	infoLog = log.New(f, "", log.Ldate|log.Ltime)
+	errorLog = log.New(f, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func main() {
+
+	if len(os.Args) <= 1 {
+		return
+	}
+
+	logConf()
+	defer f.Close()
+
+	f, err := os.OpenFile("info.log", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+	imagePath := os.Args[1]
+
+	log.SetLevel("Debug")
+
+	image, err := os.Open(imagePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	im.upload(image)
+	// url https://yandex.ru/images/touch/search?url=***&rpt=imageview&crop=0%3B0%3B1%%3B1
+	// https://yandex.ru/images/touch/search?url=https://i.imgur.com/dPrUtO2.jpg&rpt=imageview&crop=0%3B0%3B1%%3B1
+	// https://yandex.ru/images/touch/search?url=https://i.imgur.com/qMD15cz.jpg&rpt=imageview
+	browser.OpenURL(fmt.Sprintf("https://yandex.ru/images/touch/search?url=%s&rpt=imageview", im.URL))
+	time.Sleep(time.Second * 5)
+	im.delete()
+}
+func (i *imgur) upload(image io.Reader) {
+	i.init()
+
+	part, _ := i.Writer.CreateFormFile("image", "dont care about name")
 	io.Copy(part, image)
+	i.Writer.Close()
 
-	writer.Close()
-
-	req, _ := http.NewRequest("POST", "https://api.imgur.com/3/image", buf)
-	req.Header.Add("Authorization", fmt.Sprintf("Client-ID %s", conf.Client))
-	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req, _ := http.NewRequest("POST", "https://api.imgur.com/3/image", i.Buf)
+	req.Header.Add("Authorization", fmt.Sprintf("Client-ID %s", ClientID))
+	req.Header.Set("Content-Type", i.Writer.FormDataContentType())
 
 	client := &http.Client{}
 	res, _ := client.Do(req)
 	defer res.Body.Close()
-	b, _ := ioutil.ReadAll(res.Body)
-	json.Unmarshal(b, &r)
+
+	body, _ := ioutil.ReadAll(res.Body)
+	r := Request{}
+	json.Unmarshal(body, &r)
 	fmt.Printf("Operation: %s", r.Data.Link)
-	return r.Data.Link, r.Data.Deletehash
+
+	i.URL, i.Deletehash = r.Data.Link, r.Data.Deletehash
 }
-func delete(d string) {
+func (i *imgur) delete() {
+	i.init()
 
-	payload := &bytes.Buffer{}
-	writer := multipart.NewWriter(payload)
-	err := writer.Close()
+	err := i.Writer.Close()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	req, err := http.NewRequest("DELETE", fmt.Sprintf("https://api.imgur.com/3/image/%s", d), payload)
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("https://api.imgur.com/3/image/%s", i.Deletehash), i.Buf)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	req.Header.Add("Authorization", fmt.Sprintf("Client-ID %s", conf.Client))
-	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Add("Authorization", fmt.Sprintf("Client-ID %s", ClientID))
+	req.Header.Set("Content-Type", i.Writer.FormDataContentType())
 
 	client := &http.Client{}
 	res, err := client.Do(req)
